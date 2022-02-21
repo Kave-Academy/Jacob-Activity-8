501ce8dc267f0542f678062f1738adef06892404
Jacob - Kave Blocks <jacob@kaveblocks.com>
2022-02-21 11:27:03 +0800

    first commit

---
+++ Exer 8/exer.js
@@ -0,0 +1,6 @@
+function run() {
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+
+    document.getElementById("output").innerHTML =  "<h1>Width: " + myWidth + " Height: " + myHeight + "</h1>";
+}
